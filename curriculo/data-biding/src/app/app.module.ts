04c72ab73a6c0673040c8fd7edb054456996c589
Eric <maeda_Eric@hotmail.com>
2022-12-08 00:29:42 -0300

    reorganizando componentes

--- curriculo/data-biding/src/app/app.module.ts
+++ curriculo/data-biding/src/app/app.module.ts
@@ -3,14 +3,14 @@ import localePt from '@angular/common/locales/pt';
 import { LOCALE_ID, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ExercicioDataBindingComponent } from './exercicio-data-binding/exercicio-data-binding.component';
+import { ExercicioDataBindingComponent } from './Components/exercicio-data-binding/exercicio-data-binding.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
-import { ExercicioContadorComponent } from './exercicio-contador/exercicio-contador.component';
-import { ExercicioDiretirvasComponent } from './exercicio-diretirvas/exercicio-diretirvas.component';
-import { ExercicioNgClassComponent } from './exercicio-ng-class/exercicio-ng-class.component';
-import { ExercicioPipesComponent } from './exercicio-pipes/exercicio-pipes.component';
+import { ExercicioContadorComponent } from './Components/exercicio-contador/exercicio-contador.component';
+import { ExercicioDiretirvasComponent } from './Components/exercicio-diretirvas/exercicio-diretirvas.component';
+import { ExercicioNgClassComponent } from './Components/exercicio-ng-class/exercicio-ng-class.component';
+import { ExercicioPipesComponent } from './Components/exercicio-pipes/exercicio-pipes.component';
 import { parseTemplate } from '@angular/compiler';
 import { registerLocaleData } from '@angular/common';
 
