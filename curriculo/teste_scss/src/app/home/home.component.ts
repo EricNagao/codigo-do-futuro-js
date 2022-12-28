26f97182216b9f2e26804933fe240545bf6afcbd
Eric <maeda_Eric@hotmail.com>
2022-12-28 15:57:51 -0300

    master

--- curriculo/teste_scss/src/app/home/home.component.ts
+++ curriculo/teste_scss/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  usuario: any;
+
+  constructor(
+    private authService: AuthService,
+  ) { }
 
   ngOnInit(): void {
+    this.usuario = this.authService.getUsuario();
   }
 
 }
--- curriculo/teste_scss/src/app/shared/services/auth.service.ts
+++ curriculo/teste_scss/src/app/shared/services/auth.service.ts
@@ -4,17 +4,24 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
+  usuario: any;
 
   constructor() { }
 
-  setUsuario(usuaruio: any){
-      localStorage.setItem('usuario',JSON.stringify(usuaruio));
+  setUsuario(usuario: any){
+    this.usuario= usuario;
+      localStorage.setItem('usuario',JSON.stringify(usuario));
   }
 
   getUsuario(){
     const usuarioGuardado = localStorage.getItem('usuario');
-    
+
+      if(this.usuario){
+        return this.usuario;
+      }
+
       if(usuarioGuardado){
+        this.usuario =JSON.parse(usuarioGuardado);
         return JSON.parse(usuarioGuardado);
       }
       return null
